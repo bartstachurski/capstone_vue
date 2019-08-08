33f9715b45a552f069bf263a01f794fbf6a0ef8d
bartstachurski <bart.stachurski@gmail.com>
2019-08-07 22:36:06 -0400

    create google maps auto complete places search which takes lat and long from the search and sends to backend to be searched in brewery db API

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,11 @@
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
+    <input ref="autocomplete" 
+    placeholder="Seach for a location" 
+    class="search-location"
+    onfocus="value = ''" 
+    type="text" />
   </div>
 </template>
 
@@ -28,4 +33,63 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
+*, *::after, *::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: inherit;
+}
+
+body {
+  background-color: #dcdde1;
+  color: #2f3640;
+  padding: 3rem;
+}
+
+.search-location {
+  display: block;
+  width: 60vw;
+  margin: 0 auto;
+  margin-top: 5vw;
+  font-size: 20px;
+  font-weight: 400;
+  outline: none;
+  height: 30px;
+  line-height: 30px;
+  text-align: center;
+  border-radius: 10px;
+}
+
 </style>
+
+<script>
+import axios from "axios";
+
+export default {
+  mounted() {
+    this.autocomplete = new google.maps.places.Autocomplete(
+      (this.$refs.autocomplete),
+      {types: ['geocode']}
+    );
+    this.autocomplete.addListener('place_changed', () => {
+      let place = this.autocomplete.getPlace();
+      let ac = place.address_components;
+      let lat = place.geometry.location.lat();
+      let lon = place.geometry.location.lng();
+      let city = ac[0]["short_name"];
+      this.lat = lat;
+      this.lon = lon;
+      console.log(this.lat);
+      console.log(this.lon);
+      axios.get("/api/brewery_db_searches", {
+        params: {
+          lat: this.lat,
+          lng: this.lng
+        }
+      }).then(response => {
+        console.log(response.data);
+      });
+    });
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
   </div>
 </template>
 
+<style>
+
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      lat: "",
+      lon: ""
+    };
+  },
+  created: function() {},
+  methods: {}
+};
 </script>
